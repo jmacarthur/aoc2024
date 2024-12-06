ff39386aa57d87c70c1b19b4aca672e245b264ee
Jim MacArthur <jim-git@mode7.co.uk>
2024-12-06 22:25:47 +0000

    Day 6: Replace string storage with GridElements

--- day6_rust/src/main.rs
+++ day6_rust/src/main.rs
@@ -2,34 +2,33 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
 
+#[derive(PartialEq)]
+#[derive(Clone)]
+#[derive(Copy)]
+enum GridElement {
+    Blocked,
+    Free,
+}
+
 struct Grid {
-    rows: Vec<String>,
+    rows: Vec<Vec<GridElement>>,
 }
 
 impl Grid {
-    fn get(&self, x: i32, y: i32) -> Option<char> {
-        if y < 0 || y >= self.height() || x < 0 {
-            // No need to check x >= width, as nth will just return None
+    fn get(&self, x: i32, y: i32) -> Option<GridElement> {
+        if y < 0 || y >= self.height() || x < 0 || x >= self.width() {
             None
         } else {
-            self.rows[TryInto::<usize>::try_into(y).unwrap()]
-                .chars()
-                .nth(x.try_into().unwrap())
+            Some(self.rows[TryInto::<usize>::try_into(y).unwrap()][TryInto::<usize>::try_into(x).unwrap()])
         }
     }
-    fn set(&mut self, x: i32, y: i32, newchar: char) {
+    fn set(&mut self, x: i32, y: i32, newsymbol: GridElement) {
         if y < 0 || y >= self.height() || x < 0 || x>= self.width() {
             return;
         }
         let row = TryInto::<usize>::try_into(y).unwrap();
         let col = TryInto::<usize>::try_into(x).unwrap();
-        let start = &self.rows[row][..col];
-        let end = &self.rows[row][col+1..];
-        let mut newrow = String::new();
-        newrow.push_str(start);
-        newrow.push(newchar);
-        newrow.push_str(end);
-        self.rows[row] = newrow;
+        self.rows[row][col] = newsymbol;
     }
     fn width(&self) -> i32 {
         self.rows[0].len().try_into().unwrap()
@@ -42,7 +41,6 @@ impl Grid {
 enum TourResult {
     Exited { visited: usize },
     InfiniteLoop,
-    Error
 }
 
 fn evaluate_grid(grid: &Grid, start_xpos: i32, start_ypos: i32, start_direction: usize) -> TourResult {
@@ -61,17 +59,13 @@ fn evaluate_grid(grid: &Grid, start_xpos: i32, start_ypos: i32, start_direction:
         let target_x = xpos + direction_delta[direction].0;
         let target_y = ypos + direction_delta[direction].1;
         match grid.get(target_x, target_y) {
-            Some('.') | Some('X') => {
+            Some(GridElement::Free) => {
                 xpos = target_x;
                 ypos = target_y;
             },
-            Some('#') => {
+            Some(GridElement::Blocked) => {
                 direction = (direction+3)%4
             },
-            Some(e) => {
-                println!("Unrecognised character in grid {}", e);
-                return TourResult::Error;
-            }
             None => {
                 //println!("Exited grid from position {}, {}, direction {}", xpos, ypos, direction);
                 return TourResult::Exited{visited: visited.len()};
@@ -85,64 +79,64 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let row_iterator = contents.split('\n');
-    let mut row_vector: Vec<String> = vec![];
+    let mut grid_vector: Vec<Vec<GridElement>> = vec![];
     let mut start_xpos = 0i32;
     let mut ypos = 0i32;
     let mut start_ypos = 0i32;
     for row in row_iterator {
 
         if !row.is_empty() {
-            match row.find('^') {
-                Some(xpos) =>  {
-                    let fixed_str = row.replace("^", ".");
-                    row_vector.push(fixed_str);
-                    start_xpos = xpos.try_into().unwrap();
-                    start_ypos = ypos;
-                },
-                None => {
-                    row_vector.push(row.to_string());
-                }
+            let mut row_vector: Vec<GridElement> = vec![];
+            let mut xpos = 0;
+            for c in row.chars() {
+                row_vector.push(match c {
+                    '^' => {
+                        start_xpos = xpos.try_into().unwrap();
+                        start_ypos = ypos;    
+                        GridElement::Free
+                    },
+                    '.' => GridElement::Free,
+                    '#' => GridElement::Blocked,
+                    x => {
+                        panic!("Unknown source character {} at row {}, column {}", x, xpos, ypos);
+                    }
+                });
+                xpos += 1;
             }
+            grid_vector.push(row_vector);
             // Check all rows are the same length
-            assert!(row_vector[0].len() == row.len());
+            assert!(grid_vector[0].len() == row.len());
             ypos += 1;
         }
     }
-    let mut grid = Grid { rows: row_vector };
+    let mut grid = Grid { rows: grid_vector };
     println!("Starting at {},{}", start_xpos, start_ypos);
     let direction = 1;
     match evaluate_grid(&grid, start_xpos, start_ypos, direction) {
         TourResult::Exited{visited: x} => {
-            println!("Visited {} squares", x);
+            println!("Part 1: Visited {} squares", x);
         },
         TourResult::InfiniteLoop => {
             println!("Found infinite loop");
         },
-        TourResult::Error => {
-            println!("Error running tour");
-        }
     }
     // Part 2
     let mut infinite_loop_positions = 0;
     for x in 0i32..grid.width() {
         for y in 0i32..grid.height() {
-            if grid.get(x, y) == Some('.') {
-                grid.set(x, y, '#');
+            if grid.get(x, y) == Some(GridElement::Free) {
+                grid.set(x, y, GridElement::Blocked);
                 match evaluate_grid(&grid, start_xpos, start_ypos, direction) {
                     TourResult::Exited { visited: _ }=> {
                     },
                     TourResult::InfiniteLoop => {
-                        println!("Found infinite loop by replacing at {}, {}", x, y);
                         infinite_loop_positions += 1;
                     },
-                    TourResult::Error => {
-                        panic!("Error running tour");
-                    }
                 }
-                grid.set(x, y, '.');
+                grid.set(x, y, GridElement::Free);
             }
         }
     }
-    println!("That's {} new infinite loop locations", infinite_loop_positions);
+    println!("Part 2: Found {} positions which give infinite loops", infinite_loop_positions);
     Ok(())
 }
